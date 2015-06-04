b0d0884e1b7f9bba906e9b73bf44053e46191ff2
Gabor Csardi <csardi.gabor@gmail.com>
2015-06-04 14:22:42 -0400

    Simplify C++ tests, to make it work with R CMD check
    But it still does not work with devtools::check. :/

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -7,43 +7,15 @@ test_that("C++ API works", {
   inc_dir <- system.file("include", package = "progress")
   test_pkg_dir <- system.file("progresstest", package = "progress")
 
-  tmp_lib <- tempfile()
-  old_libs <- .libPaths()
-  
-  on.exit(unlink(tmp_lib, recursive = TRUE), add = TRUE)
-  on.exit(.libPaths(old_libs), add = TRUE)
-
-  dir.create(tmp_lib)
-  .libPaths(tmp_lib)
-
   ## OK, we could install it
-  R <- R.home("bin/R")
-  system(paste(R, "CMD INSTALL", "-l", tmp_lib, test_pkg_dir),
-         intern = TRUE, ignore.stderr = TRUE)
+  R <- file.path(R.home("bin"), "R")
+  install.packages(test_pkg_dir, repos = NULL, lib = .libPaths()[1],
+                   type = "source", quiet = TRUE)
   expect_true(TRUE)
 
   ## OK, we could load it
-  on.exit(unloadNamespace("progresstest"), add = TRUE)
-  library(progresstest)
+  on.exit(try(silent = TRUE, unloadNamespace("progresstest")), add = TRUE)
+  library("progresstest", character.only = TRUE, lib.loc = .libPaths()[1])
   expect_true(TRUE)
-
-  ## OK, we can even use it
-  stream <- if (is_r_studio()) "output" else "message"
-  clean <- TRUE
-  on.exit(if (clean) sink(NULL, type = stream), add = TRUE)
-
-  on.exit(if (con) { close(con) }, add = TRUE)
-  con <- FALSE
-  out_file <- tempfile()
-  con <- file(out_file, open = "w")
-  sink(con, type = stream)
-
-  progresstest::my_test_progress()
-  out <- readChar(out_file, nchars = file.info(out_file)$size)
-  sink(NULL, type = stream)
-  clean <- FALSE
-
-  expect_true(grepl("----", out))
-  expect_true(grepl("====", out))
 
 })
