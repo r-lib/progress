979173b0b2f4c8df0c4ec3975e3999b77c445b2e
Gábor Csárdi <csardi.gabor@gmail.com>
2016-11-20 20:19:41 +0000

    Revert "Remove ununed has_token field"
    This reverts commit ac56d3ab78c7649c46cb5a1f811ee74b6140d53a.

---
+++ tests/testthat/test-overhead.R
@@ -0,0 +1,68 @@
+
+context("Overhead")
+
+library(microbenchmark)
+
+test_that("overhead for short and fast loop", {
+
+  sink(tempfile())
+
+  oh <- microbenchmark(
+    without = { for (i in 1:100) { x <- sqrt(5) } },
+    with = {
+      pb <- progress_bar$new(total = 100, stream = stdout(),
+                             force = TRUE, width = 20)
+      for (i in 1:100) {
+        x <- sqrt(5)
+        pb$tick()
+      }
+    }
+  )
+
+  sink(NULL)
+
+  cat("\rOverhead, short and fast loop\n"); print(oh); cat("\n")
+})
+
+test_that("overhead for long and fast loop", {
+
+  sink(tempfile())
+
+  oh <- microbenchmark(
+    without = { for (i in 1:10000) { x <- sqrt(5) } },
+    with = {
+      pb <- progress_bar$new(total = 100, stream = stdout(),
+                             force = TRUE, width = 20)
+      for (i in 1:10000) {
+        x <- sqrt(5)
+        if (! i %% 100) pb$tick()
+      }
+    }
+  )
+
+  sink(NULL)
+
+  cat("\rOverhead, long and fast loop\n"); print(oh); cat("\n")
+})
+
+test_that("overhead for short and slow(er) loop", {
+
+  sink(tempfile())
+
+  oh <- microbenchmark(
+    times = 10,
+    without = { for (i in 1:100) { Sys.sleep(0.001) } },
+    with = {
+      pb <- progress_bar$new(total = 100, stream = stdout(),
+                             force = TRUE, width = 20)
+      for (i in 1:100) {
+        Sys.sleep(0.001)
+        pb$tick()
+      }
+    }
+  )
+
+  sink(NULL)
+
+  cat("\rOverhead, short and slow loop\n"); print(oh); cat("\n")
+})
