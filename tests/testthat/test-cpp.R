1a6840d96a70693811b5e3d668d3a04b53a3618b
Gábor Csárdi <csardi.gabor@gmail.com>
2018-04-22 09:44:27 +0100

    Simplify C++ tests

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -7,42 +7,15 @@ test_that("C++ API works", {
 
   Sys.setenv("R_TESTS" = "")
 
-  ## Need to "link to" the current package
-  test_pkg_dir <- system.file("progresstest", package = "progress")
+  dir.create(lib <- tempfile())
+  on.exit(unlink(lib, recursive = TRUE), add = TRUE)
+  install.packages("progresstest_1.0.0.tar.gz", lib = lib, quiet = TRUE)
 
-  f <- file(tempfile(), open = "w")
-  sink(f)
-  sink(f, type = "message")
-  on.exit({ sink(NULL, type = "message"); sink(NULL)})
-  suppressWarnings(
-    install.packages("Rcpp", repos = c(CRAN = "https://cran.rstudio.com"),
-                     quiet = TRUE)
-  )
-
-  sink(NULL, type = "message"); sink(NULL)
-  on.exit(NULL)
-  close(f)
-  unlink(f)
-
-  ## OK, we could install it
-  try(silent = TRUE, unloadNamespace("progresstest"))
-  install.packages(test_pkg_dir, repos = NULL, type = "source",
-                   quiet = TRUE)
-
-  ## OK, we could load it
-  expect_error(
-    do.call("library", list("progresstest", character.only = TRUE)), NA)
-  on.exit(unloadNamespace("progresstest"))
-
-  f <- file(tempfile(), open = "w")
-  sink(f)
-  sink(f, type = "message")
-  expect_error(my_test_progress(), NA)
-  sink(NULL, type = "message")
-  sink(NULL)
-  close(f)
-  unlink(f)
-
-  try(silent = TRUE, unloadNamespace("progresstest"))
-
+  on.exit(unloadNamespace("progresstest"), add = TRUE)
+  withr::with_libpaths(lib, action = "prefix", {
+    withr::with_message_sink(
+      file.path(lib, basename(tempfile())),
+      expect_error(progresstest::my_test_progress(), NA)
+    )
+  })
 })
